b6b70d42b5289e3efb4a6439b9bd58ccd673698f
Domalega <domalega.vladimir@gmail.com>
2023-07-05 01:13:32 +0700

    counter is ready

--- src/ProjectPage.tsx
+++ src/ProjectPage.tsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 
 interface ProjectPageProps {}
 
 const ProjectPage: React.FC<ProjectPageProps> = () => {
-  return <>Done</>;
+  const [counter, setCounter] = useState<number>(0);
+
+  const memoizedCounter = useMemo(() => counter, [counter]);
+
+  return (
+    <div className="d-flex align-items-center justify-content-center vh-100">
+      <div>
+        <h1 className="d-flex align-items-center justify-content-center display-1 h1">
+          Now is {memoizedCounter}
+        </h1>
+        <div className="d-flex justify-content-around">
+          <button
+            className="border-0 m-2 p-2 text-light bg-dark rounded display-5"
+            onClick={() => setCounter(counter + 1)}
+          >
+            PLUS
+          </button>
+          <button
+            className="border-1 m-2 p-2 text-light bg-dark rounded display-5"
+            onClick={() => setCounter(counter - 1)}
+          >
+            MINUS
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectPage;
